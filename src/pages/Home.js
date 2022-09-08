ca9084fa69d5481bb79be74fb243ed6e7387a8ef
Rodo <rodonguyendd@gmail.com>
2022-09-08 14:56:58 +1000

    Add content in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import rodoImage from '../media/Rodo.jpg';
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <img src={rodoImage} width="416" height="5530" alt="Rodo Picture" />
+      <p>
+        I’m a motivated gonna-be developer wanting to build tools that improve
+        human lives and solve impactful problems. I have participated in
+        multiple projects that involves building deep learning models, software
+        application, data analysis, teamwork and so on. With the accumulated
+        diverse skill-set, I’m excited to take on new challenges.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
