2df3edb8ebe56ba1e347a9da4db002c384499f76
Rodo <rodonguyendd@gmail.com>
2022-11-11 17:41:59 +1000

    Dark mode in progress

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,74 +1,63 @@
 import { Link } from "react-router-dom";
 
-const NavBar = () => {
-  
+const NavBar = ({mode, setMode}) => {
+
+  const changeMode = () => {
+    const modes = {
+      0: 'light',
+      1: 'dark',
+    }
+    // console.log('mode index:', mode, Object.values(modes).indexOf(mode))
+    setMode(modes[(Object.values(modes).indexOf(mode)+1)%2])
+    console.log('mode after clicked:', mode)
+  }
+
   return (
-  <div className="navbar-container">
-    <div className="standard-width">
-      <div>
-        <nav>
-          <Link to="/" className="navbar-item brand text-align-left">
-            Rodo
-          </Link>
+    <>
+      <div className="navbar-container">
+        <div className="standard-width">
+          <nav>
+            <Link to="/" className="navbar-item brand text-align-left">
+              Rodo
+            </Link>
             <ul>
               <li>
-                <Link to="/resume" className="navbar-item">Resume</Link>
+                <Link to="/resume" className="navbar-item">
+                  Resume
+                </Link>
               </li>
               <li>
-                <Link to="/list-100" className="navbar-item">List 100</Link>
+                <Link to="/list-100" className="navbar-item">
+                  List 100
+                </Link>
               </li>
               <li>
-                <Link to="/contactme" className="navbar-item">Contact me</Link>
+                <Link to="/contactme" className="navbar-item">
+                  Contact me
+                </Link>
               </li>
               <li>
-                <a href="https://rodonguyen.medium.com/" className="navbar-item"
+                <a
+                  href="https://rodonguyen.medium.com/"
+                  className="navbar-item"
                   target="_blank"
-                  rel="noopener noreferrer">Blog</a>
+                  rel="noopener noreferrer"
+                >
+                  Blog
+                </a>
               </li>
             </ul>
-        </nav>
+          </nav>
+        </div>
       </div>
-
-{/* 
-      <Navbar>
-      <Nav>
-        <NavItem>
-          <Link to="/" className="nav-link brand">
-            Rodo
-          </Link>
-        </NavItem>
-      </Nav>
-
-      <Nav className="right">
-        <NavItem>
-          <Link to="/resume" className="nav-link">
-            My resume
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/list-100" className="nav-link">
-            List-100
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link
-            to="https://rodonguyen.medium.com/"
-            className="nav-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Blog
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/contactme" className="nav-link">
-            Contact me
-          </Link>
-        </NavItem>
-      </Nav>
-    </Navbar> */}
-    </div>
-  </div>
-)};
+      <button type="button" className="theme-mode" onClick={() => {
+        // console.log('Mode button clicked!');
+        changeMode();
+      }}>
+        Mode
+      </button>
+    </>
+  );
+};
 
 export default NavBar;
