e41459c1477643d44e2ad63d7b69dcdcf957f237
Rodo <rodonguyendd@gmail.com>
2022-11-13 04:10:18 +1000

    Use localStorage

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,51 +1,60 @@
+import NavBar from "../components/NavBar";
+import ContactMe from "./ContactMe";
+import Project from "./Project";
+import List100 from "./List100";
+import Resume from "./Resume";
+import Home from "./Home";
+import DateMe from "./DateMe";
+import NotFound from "./NotFound";
+import "../stylesheets/App.css";
 
-import NavBar from '../components/NavBar';
-import ContactMe from './ContactMe';
-import Project from './Project';
-import List100 from './List100';
-import Resume from './Resume';
-import Home from './Home';
-import DateMe from './DateMe';
-import NotFound from './NotFound';
-import '../stylesheets/App.css';
-
-import { Outlet, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Outlet, BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 // import { Container } from 'reactstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
-  const 
-  const [themeMode, setThemeMode] = useState('Light')
+  const themeModeFromLS = localStorage.getItem("themeMode");
+  if (!themeModeFromLS) {
+    themeModeFromLS = 'Light';
+  }
+  const [themeMode, setThemeMode] = useState(themeModeFromLS);
+
+  // if (themeModeFromLS === "Dark") {
+  //   document.body.classList.add("dark");
+  //   setThemeMode("Dark");
+  // }
+
   useEffect(() => {
-    if (themeMode === 'Dark') {
+    if (themeMode === "Dark") {
       document.body.classList.add("dark");
-    } else if (themeMode === 'Light') {
+      localStorage.setItem('themeMode', 'Dark')
+    } else if (themeMode === "Light") {
       document.body.classList.remove("dark");
+      localStorage.setItem('themeMode', 'Light')
     }
   }, [themeMode]);
-  
+
   const AppConstantElements = (
     <div className="App">
-      <NavBar themeMode={themeMode} setThemeMode={setThemeMode}/>
-        <Outlet/>
+      <NavBar themeMode={themeMode} setThemeMode={setThemeMode} />
+      <Outlet />
     </div>
   );
-  
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={AppConstantElements}>
-            <Route path="contactme" element={<ContactMe/>} />
-            <Route path="projects" element={<Project/>} />
-            <Route path="resume" element={<Resume/>} />
-            <Route path="list-100" element={<List100/>} />
-            <Route path="dateme" element={<DateMe/>} />
-            <Route path="/" exact element={<Home/>} />
-            <Route path="*" element={<NotFound/>} />
+            <Route path="contactme" element={<ContactMe />} />
+            <Route path="projects" element={<Project />} />
+            <Route path="resume" element={<Resume />} />
+            <Route path="list-100" element={<List100 />} />
+            <Route path="dateme" element={<DateMe />} />
+            <Route path="/" exact element={<Home />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
