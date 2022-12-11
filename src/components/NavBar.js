4b0faf7ffa44c8dd18f34af299745bbd6230626a
Rodo <rodonguyendd@gmail.com>
2022-12-11 13:20:52 +1000

    Finish dropdown, still have bug with querySelector

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,30 @@
 import { Link } from "react-router-dom";
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 
-const NavBar = ({themeMode, setThemeMode}) => {
 
+const NavBar = ({ themeMode, setThemeMode }) => {
   const changeMode = () => {
     const modes = {
-      0: 'Light',
-      1: 'Dark',
-    }
-    // console.log('mode index:', mode, Object.values(modes).indexOf(mode))
-    setThemeMode(modes[(Object.values(modes).indexOf(themeMode)+1)%2])
-    console.log('mode after clicked:', themeMode)
-  }
+      0: "Light",
+      1: "Dark",
+    };
+    setThemeMode(modes[(Object.values(modes).indexOf(themeMode) + 1) % 2]);
+  };
+
+  const hamburgerMenuSvg = (
+    <svg
+      class="hamburger-menu"
+      width="30px"
+      height="30px"
+      viewBox="0 0 48 48"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M41,14H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,14Z" fill="#6f7380" />
+      <path d="M41,26H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,26Z" fill="#6f7380" />
+      <path d="M41,38H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,38Z" fill="#6f7380" />
+    </svg>
+  );
+  const navbarItems = document.querySelector(".navbar-items");
 
   return (
     <>
@@ -20,7 +34,7 @@ const NavBar = ({themeMode, setThemeMode}) => {
             <Link to="/" className="navbar-item brand text-align-left">
               Rodo
             </Link>
-            <ul>
+            <ul className="navbar-items">
               <li>
                 <Link to="/resume" className="navbar-item">
                   Resume
@@ -47,13 +61,28 @@ const NavBar = ({themeMode, setThemeMode}) => {
                 </a>
               </li>
             </ul>
+            <ClickAwayListener onClickAway={() => {
+              navbarItems.classList.toggle("display-vertical-nav-items");
+            }}>
+              <button
+                class="hamburger-menu navbar-item"
+                onClick={() => {
+                  navbarItems.classList.toggle("display-vertical-nav-items");
+                }}
+              >
+                {hamburgerMenuSvg}
+              </button>
+            </ClickAwayListener>
           </nav>
         </div>
       </div>
-      <button type="button" className="theme-mode" onClick={() => {
-        // console.log('Mode button clicked!');
-        changeMode();
-      }}>
+      <button
+        type="button"
+        className="theme-mode"
+        onClick={() => {
+          changeMode();
+        }}
+      >
         {themeMode} Mode
       </button>
     </>
