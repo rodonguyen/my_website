cccab6848ac77e7db15320a6924561f3ae79f5ff
Rodo <rodonguyendd@gmail.com>
2023-01-19 03:40:54 +1000

    Rename

--- src/api/coolerdate.code.js
+++ src/api/coolerdate.code.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.REACT_APP_CODE_API;
 
 // function checkExpiration(time) {
 //   const startTime = new Date(time)
