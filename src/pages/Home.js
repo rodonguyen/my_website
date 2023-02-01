d842f4abd13f88b4bd5ae09f44f15691274a0b86
Rodo <rodonguyendd@gmail.com>
2023-02-02 00:40:50 +1000

    Update styling and content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,15 +23,24 @@ const projectContents = {
     long: (
       <>
         <p>
-          Led a team of 4 to design 4 separate <b>Machine Learning models</b>{" "}
-          that can identify spam tweets with <b>{">"}95% accuracy</b> (LSTM,
-          Transformer, SVM and Naive Bayes), provided insights into each model’s
-          strengths and shortcomings&emsp;•&emsp;Evaluated the suitability for
-          real world application and Naive Bayes was the winner due to its low
-          training & inference time and high weighted-F1
-          score&emsp;•&emsp;Further reduced the computation consumption by
-          deeply analysing the dataset and removing uninformative features
-          without impacting the accuracy
+          <ul>
+            <li>
+              Led a team of 4 to design 4 separate{" "}
+              <b>Machine Learning models</b> that can identify spam tweets with{" "}
+              <b>{">"}95% accuracy</b> (LSTM, Transformer, SVM and Naive Bayes),
+              provided insights into each model’s strengths and shortcomings.
+            </li>
+            <li>
+              Evaluated the suitability for real world application and Naive
+              Bayes was the winner due to its low training & inference time and
+              high weighted-F1 score.
+            </li>
+            <li>
+              Further reduced the computation consumption by deeply analysing
+              the dataset and removing uninformative features without impacting
+              the accuracy.
+            </li>
+          </ul>
         </p>
       </>
     ),
@@ -51,13 +60,24 @@ const projectContents = {
     long: (
       <>
         <p>
-          A cryptocurrency <b>trading bot</b> programmed in Python that
-          theoretically brings +110% profit on average annually in a market
-          cycle by utilising SuperTrend indicator &emsp;•&emsp;Intensively
-          backtested BitTracker to select the best configuration and maximise
-          potential risk-adjusted return&emsp;•&emsp;I’m developing a User
-          Interface with React for an intuitive backtest history review and bot
-          operation
+          <ul>
+            <li>
+              Programmed a <b>cryptocurrency trading bot</b> in Python that can
+              guide retail investors to trade efficiently.
+            </li>
+            <li>
+              BitTracker can theoretically bring +110% profit annually on
+              average by utilising SuperTrend indicator.
+            </li>
+            <li>
+              Intensively tested BitTracker to select the best configuration and
+              maximise potential risk-adjusted return.
+            </li>
+            <li>
+              Planning to incorporate Deep Learning for smarter trading strategy
+              and build a User Interface for easier operation.
+            </li>
+          </ul>
         </p>
       </>
     ),
@@ -65,20 +85,28 @@ const projectContents = {
   3: {
     short: (
       <p>
-        A web application applying <b>auto-scaling server</b> and{" "}
-        <b>persistance services</b> that analyses keyword sentiment on Twitter
-        using Twitter REST API
+        A web application utilising <b>auto-scaling server</b> and{" "}
+        <b>persistance services</b> that analyses per-keyword sentiment on
+        Twitter using Twitter REST API
       </p>
     ),
     long: (
       <>
         <p>
-          Built a React web application with a modern user interface that
-          analyses keyword sentiment on Twitter using Twitter REST
-          API&emsp;•&emsp;Implemented <b>auto-scaling server</b> using AWS EC2,
-          Elastic Load Balancing and Auto Scaling stack to handle inconsistent
-          usage&emsp;•&emsp;Reduced latency <b>by 10 times</b> by integrating{" "}
-          <b>persistence services</b> (S3, Redis and ElastiCache)
+          <ul>
+            <li>
+              Built a React web application with a modern user interface that
+              analyses per-keyword sentiment on Twitter using Twitter REST API.
+            </li>
+            <li>
+              Implemented <b>auto-scaling server</b> using AWS EC2, Elastic Load
+              Balancing and Auto Scaling stack to handle inconsistent usage.
+            </li>
+            <li>
+              Reduced latency <b>by 10 times</b> by integrating
+              <b>persistence services</b> (S3, Redis and ElastiCache).
+            </li>
+          </ul>
         </p>
       </>
     ),
@@ -159,7 +187,7 @@ const Home = () => {
           <p>
             During my free time, I like to build pet projects where I put my
             skills or my recently acquired knowledge to practice. Don't forget
-            to pay a visit to my {' '}
+            to pay a visit to my{" "}
             {createHyperlink("https://github.com/rodonguyen", "GitHub")}
             :)
           </p>
@@ -439,10 +467,10 @@ const Home = () => {
         <h1>Skills</h1>
         <ul>
           <li>
-            <b>Languages</b>:&ensp; Python, JavaScript, C/C++/C#, Java
+            <b>Languages</b>:&ensp; Python, JavaScript, C/C++, Java, C#
           </li>
           <li>
-            <b>Data</b>:&ensp; Pandas, Numpy, Matplotlib, SQL, ETL{" "}
+            <b>Data</b>:&ensp; Pandas, Numpy, Matplotlib, SQL, ETL
           </li>
           <li>
             <b>Machine Learning</b>:&ensp; Keras, SKlearn, Pytorch, ML Model
@@ -466,12 +494,28 @@ const Home = () => {
         </ul>
 
         {/* Uncomment these when the rank/elo is high */}
-        <h2>LeetCode {' '}
-          <svg className='leetcode-icon' xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24"><path  fill="#FCC34A" d="M20.303,16.047h-9.561c-0.936,0-1.697-0.803-1.697-1.79s0.762-1.79,1.697-1.79h9.561c0.936,0,1.697,0.803,1.697,1.79S21.238,16.047,20.303,16.047z"/><path className="leetcode-icon-path-1" fill="#110638" d="M11.618,24c-1.604,0-2.977-0.533-3.97-1.541l-4.098-4.181C2.551,17.262,2,15.819,2,14.215c0-1.578,0.551-3.008,1.552-4.025l9.519-9.681c0.66-0.67,1.828-0.653,2.506,0.036c0.694,0.706,0.71,1.839,0.034,2.524l-1.762,1.816c0.655,0.268,1.241,0.659,1.739,1.159l2.463,2.53c0.672,0.684,0.655,1.815-0.039,2.521c-0.346,0.352-0.802,0.545-1.284,0.545l0,0c-0.464,0-0.896-0.181-1.219-0.509l-2.536-2.492c-0.321-0.327-0.779-0.49-1.367-0.49c-0.606,0-1.069,0.157-1.375,0.469l-4.067,4.194c-0.342,0.349-0.521,0.831-0.521,1.4c0,0.577,0.189,1.101,0.519,1.436l4.083,4.182c0.315,0.321,0.774,0.484,1.362,0.484s1.045-0.163,1.36-0.484l2.549-2.505c0.314-0.321,0.746-0.502,1.209-0.503c0.001,0,0.002,0,0.002,0c0.483,0,0.939,0.194,1.286,0.546c0.693,0.705,0.71,1.837,0.036,2.522l-2.457,2.525C14.586,23.438,13.176,24,11.618,24z M14.29,1c-0.193,0-0.374,0.074-0.507,0.21l-9.519,9.681C3.449,11.72,3,12.9,3,14.215c0,1.341,0.449,2.535,1.265,3.363c0,0,0,0,0.001,0.001l4.097,4.18C9.162,22.57,10.288,23,11.618,23c1.288,0,2.444-0.455,3.258-1.282l2.457-2.525c0.295-0.301,0.279-0.804-0.034-1.122c-0.156-0.159-0.36-0.247-0.573-0.247c0,0,0,0-0.001,0c-0.192,0.001-0.37,0.075-0.502,0.209l-2.549,2.505c-0.497,0.507-1.214,0.778-2.068,0.778s-1.572-0.271-2.076-0.784L5.446,16.35c-0.519-0.527-0.805-1.286-0.805-2.136c0-0.824,0.286-1.57,0.806-2.099l4.067-4.194c0.503-0.512,1.206-0.771,2.091-0.771c0.854,0,1.571,0.271,2.074,0.783l2.536,2.492c0.139,0.142,0.318,0.216,0.512,0.216l0,0c0.212,0,0.415-0.087,0.571-0.246c0.313-0.319,0.33-0.822,0.037-1.121l-2.461-2.528c-0.56-0.563-1.263-0.957-2.028-1.137c-0.175-0.041-0.331-0.176-0.382-0.349s-0.021-0.363,0.104-0.492l2.325-2.398c0.298-0.302,0.282-0.805-0.031-1.124C14.707,1.088,14.504,1,14.29,1z"/></svg>
+        <h2>
+          LeetCode{" "}
+          <svg
+            className="leetcode-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            enable-background="new 0 0 24 24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#FCC34A"
+              d="M20.303,16.047h-9.561c-0.936,0-1.697-0.803-1.697-1.79s0.762-1.79,1.697-1.79h9.561c0.936,0,1.697,0.803,1.697,1.79S21.238,16.047,20.303,16.047z"
+            />
+            <path
+              className="leetcode-icon-path-1"
+              fill="#110638"
+              d="M11.618,24c-1.604,0-2.977-0.533-3.97-1.541l-4.098-4.181C2.551,17.262,2,15.819,2,14.215c0-1.578,0.551-3.008,1.552-4.025l9.519-9.681c0.66-0.67,1.828-0.653,2.506,0.036c0.694,0.706,0.71,1.839,0.034,2.524l-1.762,1.816c0.655,0.268,1.241,0.659,1.739,1.159l2.463,2.53c0.672,0.684,0.655,1.815-0.039,2.521c-0.346,0.352-0.802,0.545-1.284,0.545l0,0c-0.464,0-0.896-0.181-1.219-0.509l-2.536-2.492c-0.321-0.327-0.779-0.49-1.367-0.49c-0.606,0-1.069,0.157-1.375,0.469l-4.067,4.194c-0.342,0.349-0.521,0.831-0.521,1.4c0,0.577,0.189,1.101,0.519,1.436l4.083,4.182c0.315,0.321,0.774,0.484,1.362,0.484s1.045-0.163,1.36-0.484l2.549-2.505c0.314-0.321,0.746-0.502,1.209-0.503c0.001,0,0.002,0,0.002,0c0.483,0,0.939,0.194,1.286,0.546c0.693,0.705,0.71,1.837,0.036,2.522l-2.457,2.525C14.586,23.438,13.176,24,11.618,24z M14.29,1c-0.193,0-0.374,0.074-0.507,0.21l-9.519,9.681C3.449,11.72,3,12.9,3,14.215c0,1.341,0.449,2.535,1.265,3.363c0,0,0,0,0.001,0.001l4.097,4.18C9.162,22.57,10.288,23,11.618,23c1.288,0,2.444-0.455,3.258-1.282l2.457-2.525c0.295-0.301,0.279-0.804-0.034-1.122c-0.156-0.159-0.36-0.247-0.573-0.247c0,0,0,0-0.001,0c-0.192,0.001-0.37,0.075-0.502,0.209l-2.549,2.505c-0.497,0.507-1.214,0.778-2.068,0.778s-1.572-0.271-2.076-0.784L5.446,16.35c-0.519-0.527-0.805-1.286-0.805-2.136c0-0.824,0.286-1.57,0.806-2.099l4.067-4.194c0.503-0.512,1.206-0.771,2.091-0.771c0.854,0,1.571,0.271,2.074,0.783l2.536,2.492c0.139,0.142,0.318,0.216,0.512,0.216l0,0c0.212,0,0.415-0.087,0.571-0.246c0.313-0.319,0.33-0.822,0.037-1.121l-2.461-2.528c-0.56-0.563-1.263-0.957-2.028-1.137c-0.175-0.041-0.331-0.176-0.382-0.349s-0.021-0.363,0.104-0.492l2.325-2.398c0.298-0.302,0.282-0.805-0.031-1.124C14.707,1.088,14.504,1,14.29,1z"
+            />
+          </svg>
         </h2>
         <ul>
           <li>
-            <b>Rank</b>: 1,000,000
+            <b>Rank</b>: 924,500
           </li>
           <li>
             <b>Profile</b>:{" "}
