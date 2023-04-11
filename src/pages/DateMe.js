55892688f895975c822ee9040fa76b41e614ea9a
Rodo <rodonguyendd@gmail.com>
2023-04-11 10:25:55 +1000

    Mics

--- src/pages/DateMe.js
+++ src/pages/DateMe.js
@@ -8,6 +8,7 @@ import { addRespondentFormToDatabase } from "../api/coolerdate.respondent";
 
 import NotFound from "./NotFound";
 import CoolerDateEndPage from "../components/CoolerDate/CoolerDateEndPage";
+import { calculateTimeLeftInSeconds, formatTimeLeft } from "../components/DateMe.utils";
 // import CoolerDateForm from "../components/CoolerDate/CoolerDateForm";
 // import { Input, Button, Form, Label } from "reactstrap";
 
@@ -118,47 +119,7 @@ const DateMe = () => {
   // Update timeLeft
   useEffect(function () {
     // Block if myCheckResult has not been received and updated
-    if (!myCheckResult || myCheckResult.data.isValid === false) {
-      console.log('Block update timeLeft.')
-      return
-    }
-
-    /** Add leading zeros to a number*/
-    function addLeadingZeros(number, size = 2) {
-      number = number.toString();
-      while (number.length < size) number = "0" + number;
-      return number;
-    }
-
-    /** Format timeLeft */
-    function formatTimeLeft(timeLeftInSeconds) {
-      timeLeftInSeconds = timeLeftInSeconds - 1;
-      const hours = addLeadingZeros(Math.floor(timeLeftInSeconds / (60 * 60)));
-      const minutes = addLeadingZeros(Math.floor(timeLeftInSeconds / 60) % 60);
-      const seconds = addLeadingZeros(timeLeftInSeconds % 60);
-
-      const timeLeftInString = `${hours}h:${minutes}m:${seconds}s`;
-      return timeLeftInString;
-    }
-
-    /** Calculate 'timeLeft' for the 'code' */ 
-    function calculateTimeLeftInSeconds(firstAccessTime, milisecondsGivenTillExpiration){
-      const startDatetime = new Date(firstAccessTime).getTime();
-      // console.log(startDatetime)
-      const now = new Date().getTime();
-      const difference = startDatetime + milisecondsGivenTillExpiration - now
-
-      // Stop calculating if time out
-      if (difference <= 0) {
-        // Could add code to direct to homepage immediately after code is expired but  
-        // user are allowed send their response if they keep staying in the webpage.
-        // The server does not have the logic to block expired code from sending response anyway.
-        console.log('difference <= 0')
-        return 0
-      }
-      const timeLeftInSeconds = Math.floor(new Date().setTime(difference) / 1000);     
-      return timeLeftInSeconds;
-    }
+    if (!myCheckResult || myCheckResult.data.isValid === false) return
     
     // Assign firstAccessTime a `Date` of 'currentDateTime' if this is the first time accessing 
     // (i.e. myCheckResult.data.entry.firstAccessTime is `null`) 
@@ -176,8 +137,6 @@ const DateMe = () => {
     return () => clearTimeout(timer);
   // eslint-disable-next-line
   }, [secondsLeftUntilCodeExpires, myCheckResult, currentDateTime]);
-
-
 
 
 
