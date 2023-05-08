85623dc76218dd13cfbe77e0d07d3f9c04e32261
Rodo <rodonguyendd@gmail.com>
2023-05-09 00:01:25 +1000

    Initialise Typescript, add simple tsx file

---
+++ src/pages/TimeIHaveLeft.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const TimeIHaveLeft: React.FC = () => {
+  return (
+    <>
+      <h1> Time I Have Left </h1> 
+
+
+
+    </>
+  );
+};
+
+export default TimeIHaveLeft;
