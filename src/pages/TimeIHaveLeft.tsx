25a7d526c63422faf2b7ab9a73968a67fc7675ea
Rodo <rodonguyendd@gmail.com>
2023-06-07 16:25:16 +1000

    Clean

--- src/pages/TimeIHaveLeft.tsx
+++ src/pages/TimeIHaveLeft.tsx
@@ -5,16 +5,12 @@ const TimeIHaveLeft: React.FC = () => {
   const calculateDaysDifference = (inputDateString: string): number => {
     const currentDate = new Date();
     const inputDate = new Date(inputDateString);
-
     // Add 80 years - the expected life expectancy
     inputDate.setFullYear(inputDate.getFullYear() + 80);
-
     // Calculate the difference in milliseconds
     const differenceMs = inputDate.getTime() - currentDate.getTime();
-
     // Convert the difference to days
     const differenceDays = Math.floor(differenceMs / (1000 * 60 * 60 * 24));
-
     return differenceDays;
   };
 
@@ -38,38 +34,13 @@ const TimeIHaveLeft: React.FC = () => {
           <input type="date" name="dob" min="1800-01-01" max="2100-01-01" defaultValue='2001-01-01' required></input><br></br>
           <input type="submit" value="Calculate"></input><br></br>
         </form>
+
+        <p>If you can live up to 80 years old, then...</p>
         <p>Days left: {daysLeft}</p>
 
       </div>
       </>
     );
   };
-
-        {/* <label for="contact" required>Your contact *</label><br></br>
-        <input type="text" id="coolerdate" name="contact" placeholder="Email is fine :)" ></input><br></br>
-
-        <label for="bio" required>Something about you *</label><br></br>
-        <input type="text" id="coolerdate" name="bio" ></input><br></br>
-
-        <label for="ifact">Interesting facts not many people know about you</label><br></br>
-        <input type="text" id="coolerdate" name="ifact"></input><br></br>
-
-        <label for="place">An ideal first-date place? It can be general 
-          (e.g. cafe, dinner, river view, romantic atmosphere) or specific 
-          (e.g. ABC Restaurant)?</label><br></br>
-        <input type="text" id="coolerdate" name="place"></input><br></br>
-
-        <label for="dressing" >How do you want me to dress in our first date</label><br></br>
-        <input type="text" id="coolerdate" name="dressing"></input><br></br>
-
-        <label for="boyfriend">3 words to describe your desired boyfriend?</label><br></br>
-        <input type="text" id="coolerdate" name="boyfriend"></input><br></br>
-
-
-        {!first3SectionsFilled && (
-          <label className="fade">
-            Please fill the required sections (*) before sending
-          </label>
-        )} */}
-
+  
 export default TimeIHaveLeft;
