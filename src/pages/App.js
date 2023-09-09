a1eab1b1af8f85c5f723e6cea920bf6fea074df7
Rodo <rodo@aervision.com>
2023-09-09 18:54:11 +1000

    Migrate TimeIHaveLeft to Amazing subdomain

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,7 +4,6 @@ import List100 from "./List100";
 import Resume from "./Resume";
 import Home from "./Home";
 import DateMe from "./DateMe";
-import TimeIHaveLeft from "./TimeIHaveLeft.tsx";
 import NotFound from "./NotFound";
 import "../stylesheets/App.css";
 
@@ -42,7 +41,6 @@ function App() {
             <Route path="resume" element={<Resume />} />
             <Route path="list-100" element={<List100 />} />
             <Route path="dateme" element={<DateMe />} />
-            <Route path="timeihaveleft" element={<TimeIHaveLeft/>} />
             <Route path="/" exact element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Route>
