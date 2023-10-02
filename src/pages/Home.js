a63cbb7256984d7c61a3956047fc64e3849e6f6e
Rodo <rodonguyendd@gmail.com>
2023-10-02 19:08:52 +1000

    Migrated Projects and Improved styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 //
 import rodoImage from '../media/Rodo.jpg';
-import Projects from '../components/Home/Projects';
 import History from '../components/Home/History';
 import createHyperlink from '../components/utils';
 
@@ -18,16 +17,16 @@ const Home = () => {
 					<h1 className='xlarge text-align-left'>Hello World!</h1>
 					<p>
 						I’m a motivated Sofware Engineer / Machine Learning Engineer who loves to
-						work on impactful, interesting, data-driven, AI-integrated products that
-						bring value to users at scale. Equipped with a diverse skill set, from
-						software development, data analysis to deep learning model design, I’m
-						excited to take on new challenges.
+						work on impactful, AI-integrated products that bring value to users at
+						scale. Equipped with a diverse skill set, from software development, data
+						analysis to deep learning model design, I’m excited to take on new
+						challenges.
 					</p>
 					<p>
-						On the personal side I'm working on creating a J.A.R.V.I.S for me one day :)
-						During my free time, I like to build pet projects where I put my skills or
-						my recently acquired knowledge to practice. While we're on this, read about
-						pet projects below or in Resume tab.
+						I'm working on creating my own J.A.R.V.I.S :) During my free time, I like to
+						build pet projects where I put my skills or my recently acquired knowledge
+						to practice. While we're on this, have a look at{' '}
+						{createHyperlink('https://amazing.rodonguyen.dev/', 'my pet projects')}.
 					</p>
 					<p>#software #data #machinelearning #blockchain</p>
 				</div>
@@ -75,7 +74,11 @@ const Home = () => {
 			{/* content block: Projects */}
 			<div className='background-color-2'>
 				<div className='container'>
-					<Projects />
+					<h1>Amazing Projects</h1>
+					{createHyperlink(
+						'https://amazing.rodonguyen.dev/',
+						<button id='amazing-webpage-button'>{'<Check them out/>'}</button>
+					)}
 				</div>
 			</div>
 
@@ -112,11 +115,11 @@ const Home = () => {
 
 			<div className='background-color-2'>
 				<div className='container'>
-					<h1>Mics</h1>
+					<h1>Hobbies</h1>
 
 					{/* Uncomment these when the rank/elo is high */}
-					<h2>
-						LeetCode{' '}
+					<p>
+						{createHyperlink('https://leetcode.com/rodonguyen/', 'Leetcode')}{' '}
 						<svg
 							className='leetcode-icon'
 							xmlns='http://www.w3.org/2000/svg'
@@ -133,27 +136,14 @@ const Home = () => {
 								d='M11.618,24c-1.604,0-2.977-0.533-3.97-1.541l-4.098-4.181C2.551,17.262,2,15.819,2,14.215c0-1.578,0.551-3.008,1.552-4.025l9.519-9.681c0.66-0.67,1.828-0.653,2.506,0.036c0.694,0.706,0.71,1.839,0.034,2.524l-1.762,1.816c0.655,0.268,1.241,0.659,1.739,1.159l2.463,2.53c0.672,0.684,0.655,1.815-0.039,2.521c-0.346,0.352-0.802,0.545-1.284,0.545l0,0c-0.464,0-0.896-0.181-1.219-0.509l-2.536-2.492c-0.321-0.327-0.779-0.49-1.367-0.49c-0.606,0-1.069,0.157-1.375,0.469l-4.067,4.194c-0.342,0.349-0.521,0.831-0.521,1.4c0,0.577,0.189,1.101,0.519,1.436l4.083,4.182c0.315,0.321,0.774,0.484,1.362,0.484s1.045-0.163,1.36-0.484l2.549-2.505c0.314-0.321,0.746-0.502,1.209-0.503c0.001,0,0.002,0,0.002,0c0.483,0,0.939,0.194,1.286,0.546c0.693,0.705,0.71,1.837,0.036,2.522l-2.457,2.525C14.586,23.438,13.176,24,11.618,24z M14.29,1c-0.193,0-0.374,0.074-0.507,0.21l-9.519,9.681C3.449,11.72,3,12.9,3,14.215c0,1.341,0.449,2.535,1.265,3.363c0,0,0,0,0.001,0.001l4.097,4.18C9.162,22.57,10.288,23,11.618,23c1.288,0,2.444-0.455,3.258-1.282l2.457-2.525c0.295-0.301,0.279-0.804-0.034-1.122c-0.156-0.159-0.36-0.247-0.573-0.247c0,0,0,0-0.001,0c-0.192,0.001-0.37,0.075-0.502,0.209l-2.549,2.505c-0.497,0.507-1.214,0.778-2.068,0.778s-1.572-0.271-2.076-0.784L5.446,16.35c-0.519-0.527-0.805-1.286-0.805-2.136c0-0.824,0.286-1.57,0.806-2.099l4.067-4.194c0.503-0.512,1.206-0.771,2.091-0.771c0.854,0,1.571,0.271,2.074,0.783l2.536,2.492c0.139,0.142,0.318,0.216,0.512,0.216l0,0c0.212,0,0.415-0.087,0.571-0.246c0.313-0.319,0.33-0.822,0.037-1.121l-2.461-2.528c-0.56-0.563-1.263-0.957-2.028-1.137c-0.175-0.041-0.331-0.176-0.382-0.349s-0.021-0.363,0.104-0.492l2.325-2.398c0.298-0.302,0.282-0.805-0.031-1.124C14.707,1.088,14.504,1,14.29,1z'
 							/>
 						</svg>
-					</h2>
-					<ul>
-						<li>
-							<b>Rank</b>: 700,000
-						</li>
-						<li>
-							<b>Profile</b>:{' '}
-							{createHyperlink('https://leetcode.com/rodonguyen/', 'rodonguyen')}
-						</li>
-					</ul>
-
-					<h2>Chess - Beginner 😝 </h2>
-					<ul>
-						<li>
-							<b>ELO</b>: ~1000 (Rapid), ~800 (Blitz)
-						</li>
-						<li>
-							<b>Profile</b>:{' '}
-							{createHyperlink('https://www.chess.com/member/rodobolo', 'rodobolo')}
-						</li>
-					</ul>
+						&ensp; Top 700k
+					</p>
+					<p>
+						{createHyperlink('https://www.chess.com/member/rodobolo', 'Chess')} ♟️
+						Beginner, ELO 1000 (rapid){' '}
+					</p>
+					<p>Hiking, Gym/Calisthenics, Bawling, Badminton, Reading, Movies</p>
+					<p>Haven't tried but willing to: Camping, Tennis,...</p>
 				</div>
 			</div>
 
@@ -162,17 +152,16 @@ const Home = () => {
 				<p>p.s. </p>
 				<p>
 					This website is made with bare HTML/CSS, ReactJS and no special styling
-					frameworks :){' '}
+					frameworks :) KISS!
 				</p>
-				<p>Frameworks come and go. Only the code we write stays.</p>
 			</div>
 
 			{/* Certificates section
-      <div className="background-color-2">
-        <div className="container text-align-left certificate">
-          <h1>Certificates</h1>
-        </div>
-      </div> */}
+			<div className="background-color-2">
+				<div className="container text-align-left certificate">
+				<h1>Certificates</h1>
+				</div>
+			</div> */}
 		</>
 	);
 };
--- src/pages/List100.js
+++ src/pages/List100.js
@@ -8,47 +8,50 @@ const List100 = () => {
       <h1 className="text-align-left xlarge">List 100</h1>
       <p>We all have 1 life. Why not making the most of it and do exciting things? These are the things that keep me excitedly waking up every morning and that I plan to do before I die. Inspired by Chip Huyen's.</p>
       <ol className="text-align-left">
+        {/* Legacy */}
         <li>✗ Read 100 books ({readBookNum}/100)</li>
+        <li>✗ Read 1000 books ({readBookNum}/1000)</li>
+        <li>✓ Teach a course in university</li>
+        <li>✗ Found a tech company in Vietnam and it survives :)</li>
+        <li>✗ Give a talk to {'>'}100 audiences (like... any talk)</li>
+        <li>✗ Establish a Mentorship Program in my high school</li>
+        <li>✗ Found an organic modern farm (my father's dream)</li>
+        {/* Travelling */}
         <li>✗ See Auroras</li>
         <li>✗ Conquer Fansipan Mountain</li>
         <li>✗ 10-day Vipassana Meditation Retreat</li>
-        <li>✓ Teach a course in university</li>
         <li>✗ Pay and have a 14-day holiday with my family</li>
-        <li>✗ Annual income before tax AU$65,000</li>
-        <li>✗ Found a tech company in Vietnam and it survives :)</li>
-        <li>✗ Give a talk to {'>'}100 audiences (like... any talk)</li>
         <li>✗ Visit Norway's mountains</li>
+        <li>✗ Visit New Zealand's mountains</li>
         <li>✗ Travel to 10 countries (1/10)</li>
         <li>✗ Travel to 20 Vietnamese cities (5/20)</li>   {/* HCM, Danang, Hue, Phu Quoc, Kon Tum,  */}
+        <li>✗ Have a vacation on an island/a yatch</li>
+        <li>✗ Climb Great Pyramid of Giza, Egypt</li>
+        <li>✗ Travel to Africa</li>
+        <li>✗ Sky dive (without an instructor)</li>
+        <li>✗ Camp and enjoy the Milky Way Galaxy</li>
+        <li>✗ Hike to Everest base camp + see the Hymalayas</li>
+        <li>✗ Cuddle lions</li>
+        <li>✗ Ride horse</li>
+        {/* Career */}
+        <li>✓ Annual income before tax AU$65,000</li>
         <li>✗ Get a PhD (I would be the 1st Dr in my family)</li>
         <li>✗ Get Aussie PR</li>
         <li>✗ Buy a house for my parents to retire in Australia</li>
         <li>✗ Get a driver license</li>
         <li>✗ Learn to fly helicopter/plane</li>
-        <li>✗ Be Financial Independent</li>
-        <li>✗ Read 1000 books ({readBookNum}/1000)</li>
-        <li>✗ Invest AU$1,000,000</li>
-        <li>✗ Have a vacation on an island/a yatch</li>
-        <li>✗ Work in a big financial insitution</li>
-        <li>✗ Climb Great Pyramid of Giza, Egypt</li>
-        <li>✗ Sky dive (without an instructor)</li>
-        <li>✗ Camp and enjoy the Milky Way Galaxy</li>
-        <li>✗ Hike to Everest base camp + see the Hymalayas</li>
+        <li>✗ Achieve Financial Freedom</li>
+        <li>✗ Work in a giant financial insitution</li>
         <li>✗ Learn to play a musical instrument (guitar)</li>
         <li>✗ Ask a stranger out successfully</li>
         <li>✗ Finish a full marathon</li>
         <li>✗ Be 70kg and muscular</li>
-        <li>✗ Cuddle lions</li>
         <li>✗ Master NeoVim</li>
+        <li>✗ Connect with 10 start-up founders</li>
         <li>✗ Walk on the moon</li>
-        <li>✗ Swim in Exosphere in a space suit</li>
+        <li>✗ Swim in outer space</li>
         <li>✗ Date a model</li>
         <li>✗ Swim with whales/dolphins</li>
-        {/* <li>✗ </li> */}
-        <li>✗ Travel to Africa</li>
-        <li>✗ Connect with 10 start-up founders</li>
-        <li>✗ Establish a Mentorship Program in my high school</li>
-        <li>✗ Found an organic modern farm (my father's dream)</li>
         <li>~ Live a great life</li>
         {/* <li>✓ </li> */}
         {/* <li>✗ Bring a girl home from club</li> */}
@@ -56,11 +59,11 @@ const List100 = () => {
       </ol>
 
       <h1 className="text-align-left xlarge">My Wish List</h1>
-      <p> This helps mitigate the headache of my close friends as my birthday comes. Love, Rodo{' <3'}</p>
+      <p> This helps mitigate the headache of my close friends as my birthday comes. {'Love, Rodo <3'}</p>
       <ul className="text-align-left">
         <li>UNIQLO or any giftcard</li>
-        <li>UltraBoost shoes (in a color that's suitable for both casual and formal, size 10.5)</li>
-        <li>Clothes: {createHyperlink('https://au.shop.gymshark.com/collections/tanks/mens', 'tank top for gym')}, etc. </li>
+        <li>UltraBoost shoes (size 11)</li>
+        <li>Gym clothes: {createHyperlink('https://au.shop.gymshark.com/collections/tanks/mens', 'tank top for gym')} or anything else will do. </li>
         <li>Nothing else for now, I'm quite a minimalist :)</li>
       </ul>
     </div>
