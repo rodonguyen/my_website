3bcd8d155c0ca10cd163488f178f1e8d586f572f
Rodo <rodo@aervision.com>
2023-10-03 12:36:11 +1000

    Cleaned, Added dev.to

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -3,18 +3,19 @@ import createHyperlink from '../components/utils';
 
 const ContactMe = () => {
   return (
-  <div className="container contact-list">
-      <h1 className='center'>
-      I love deep conversations.
-      </h1>
-      
-      <ul className='text-align-left center'>
-        <li>Email: rodonguyendd at gmail.com </li>
-        <li>{createHyperlink('https://www.linkedin.com/in/rodonguyen/','LinkedIn')}</li>
-        <li>{createHyperlink('https://github.com/rodonguyen/','Github')}</li>
-        <li>{createHyperlink('https://rodonguyen.medium.com/','Medium')}</li>
-      </ul>
-  </div>
+    <div className="container contact-list">
+        <h1 className='center'>
+          I love deep conversations
+        </h1>
+        
+        <ul className='text-align-left center'>
+          <li>Email: rodonguyendd at gmail.com </li>
+          <li>{createHyperlink('https://www.linkedin.com/in/rodonguyen/','LinkedIn')}</li>
+          <li>{createHyperlink('https://github.com/rodonguyen/','Github')}</li>
+          <li>{createHyperlink('https://dev.to/rodonguyen','Dev.to')}</li>
+          <li>{createHyperlink('https://rodonguyen.medium.com/','Medium')}</li>
+        </ul>
+    </div>
   );
 };
 
