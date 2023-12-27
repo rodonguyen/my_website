0946637e0d55a24913cbf9be1c9c1ff0a3c2bd71
Rodo <rodonguyendd@gmail.com>
2023-12-27 17:05:14 +1000

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+// https://www.realtimecolors.com/?colors=e8f3f3-030307-0670c1-329bec-294ef5&fonts=Poppins-Poppins
 module.exports = {
 	content: ["./src/**/*.{js,jsx}", "./public/index.html"],
 	darkMode: "class",
@@ -16,7 +18,7 @@ module.exports = {
 					DEFAULT: "#137DCE",
 					dark: "#329bec"
 				},
-				accent: "#274cf5",
+				accent: "#294EF5",
 			},
 			textColor: {
 				"custom-black": "#0C1717",
