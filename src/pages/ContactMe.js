a1a71fb72fa87769853a3ee43ea9f6c04170e49d
Rodo <rodonguyendd@gmail.com>
2024-07-14 15:57:38 +1000

    chore

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -7,9 +7,10 @@ const ContactMe = () => {
 		<div className="container contact-list text-align-left">
 			<h1 className="quote">"A stranger is a friend we haven't met."</h1>
 			<p className="author text-align-right"> - a marketplace buyer</p>
+      <br/>
 			{createHyperlink("https://www.linkedin.com/in/rodonguyen/", "LinkedIn")} /{" "}
 			{createHyperlink("https://twitter.com/rodonguyen", "X")} /{" "}
-			{createHyperlink("https://github.com/rodonguyen/", "Github")} / Email: rodonguyendd at
+			{createHyperlink("https://github.com/rodonguyen/", "Github")} / rodonguyendd at
 			gmail.com
 		</div>
 	)
