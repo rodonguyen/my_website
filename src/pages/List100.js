6868c7edf8e7035de3766233880180baefa4b7e9
Rodo <rodonguyendd@gmail.com>
2024-08-08 22:12:55 +1000

    update: list100 and CV

--- src/pages/List100.js
+++ src/pages/List100.js
@@ -19,7 +19,7 @@ const List100 = () => {
 				<li>✓ Teach a course in university (CAB403, CAB230, IFB102 at QUT)</li>
 				<li>✗ Found a tech company in Vietnam and it survives :)</li>
 				<li>✗ Give a talk to {">"}100 audiences (like... any talk)</li>
-				<li>✗ Establish a Mentorship Program in my high school</li>
+				<li>✗ Establish a Mentorship Program/AlumNetwork in my high school</li>
 				<li>✗ Found an organic modern farm (my father's dream)</li>
 				{/* Travelling */}
 				<li>✗ See Auroras</li>
@@ -42,7 +42,7 @@ const List100 = () => {
 				{/* Career */}
 				<li>✓ Annual income before tax AU$65,000</li>
 				<li>✗ Get a PhD (I would be the 1st Dr in my family)</li>
-				{/* <li>✗ Get Aussie PR</li> */}
+				<li>✗ Get Aussie PR</li>
 				<li>✗ Buy a house for my parents to retire in Australia</li>
 				<li>✗ Get a driver license</li>
 				<li>✗ Learn to fly helicopter/plane</li>
@@ -52,16 +52,15 @@ const List100 = () => {
 				<li>✗ Ask a stranger out successfully</li>
 				<li>✗ Finish a full marathon</li>
 				<li>✗ Be 70kg and muscular</li>
-				<li>✗ Master NeoVim</li>
-				<li>✗ Connect with 10 start-up founders</li>
+				<li>✗ Master Vim</li>
+				<li>✗ Connect with 10 start-up founders (GPIB, RSP, Electrafi, AerVision, Concensus Layer, Labrys)  </li>
 				<li>✗ Walk on the moon</li>
 				<li>✗ Swim in outer space</li>
-				<li>✗ Date a model</li>
 				<li>✗ Swim with whales/dolphins</li>
 				<li>~ Live a great life</li>
+				{/* <li>✗ Date a model</li> */}
 				{/* <li>✓ </li> */}
 				{/* <li>✗ Bring a girl home from club</li> */}
-				{/* <li>✗ Bodycount = 10</li> */}
 			</ol>
 
 			<h1 className="text-align-left xlarge">My Wish List</h1>
@@ -71,11 +70,9 @@ const List100 = () => {
 			</p>
 			<ul className="text-align-left">
 				<li>Polaroid camera/printer</li>
-				<li>Sport/UltraBoost shoes (US 10, FR 44)</li>
-				<li>Tennis racket</li>
-				<li>Great time with friends and family.</li>
+				<li>Great time with friends and family</li>
 				<li>Swisse watch with EOL battery (Tissot? maybe)</li>
-				<li>1 Bitcoin (haha)</li>
+				<li>Some Sats / Eth</li>
 			</ul>
 		</div>
 	)
